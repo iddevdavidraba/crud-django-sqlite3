3d7efe5dd837ddf30be6967c2b6087eee2978f1c
iddevdavidraba <iddev.davidraba@gmail.com>
2022-05-24 02:37:34 -0500

    add eliminate function in button

--- Universidad/Aplicaciones/Academico/views.py
+++ Universidad/Aplicaciones/Academico/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Curso
 # Create your views here.
+
 
 def home(request):
     cursosListados = Curso.objects.all()
     return render(request, 'gestionCursos.html', {'cursos': cursosListados})
+
+
+def registrarCurso(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    creditos = request.POST['numCreditos']
+
+    curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+    return redirect('/')
+
+
+def eliminarCurso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    curso.delete()
+    return redirect('/')
